06c266a0fe29d7afd9570e93fbfb80b8d56199bd
Alex Schworer <alex.schworer@gmail.com>
2010-08-21 13:39:27 -0700

    add test case

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import unittest
+import homport
+homport.bootstrap()
 
 class NodeWrapTestCase(unittest.TestCase):
     def setUp(self):
-        self.assert_('hou' in globals())
-        import homport
-        homport.bootstrap()
+        self.assertTrue('hou' in globals())
 
     def testWrapped(self):
+        self.assertTrue(isinstance(hou.node('/obj'), homport.NodeWrap))
+
+    def testGetNode(self):
         hou.node('/obj').createNode('geo')
         self.assertEquals(hou.node('/obj/geo1').node, hou.node('/obj').geo1.node)
-
 
 if __name__ == "__main__":
     unittest.main()
