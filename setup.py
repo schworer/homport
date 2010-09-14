4add4c83a0fd48030102a33cbb17e32597b2406f
Alex Schworer <alex.schworer@gmail.com>
2010-09-13 21:58:44 -0700

    make it installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = "0.1"
+
+setup(name="homport",
+      version=version,
+      description="Wrapper for interacting with Houdini nodes",
+      author="Alex Schworer",
+      author_email="alex.schworer@gmail.com",
+      url="http://schworer.github.com/homport",
+      py_modules=["homport"])
